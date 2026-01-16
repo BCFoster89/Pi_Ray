907828479d50411c3b17ed3110d2f31f343d7f1d
BCFoster89 <47796060+BCFoster89@users.noreply.github.com>
2026-01-16 13:48:14 -0500

    Paste file start
    Removed global print override and test code.

--- rov_clean/logger.py
+++ rov_clean/logger.py
@@ -1,3 +1,4 @@
+# logger.py
 import time, sys
 
 log_buffer = []
@@ -9,11 +10,3 @@ def log(msg):
     if len(log_buffer) > 300:
         log_buffer.pop(0)
     sys.__stdout__.write(entry + "\n")
-
-# Override print globally here
-print = log
-
-if __name__ == "__main__":
-    log("Logger test message.")
-    print("Another test (overridden print).")
-    print("Buffer length:", len(log_buffer))
